c2e29de7d854f684b19b110a855de428b02b0817
Anishkumar-26 <anishkumar56743@gmail.com>
2026-01-28 14:40:00 +0530

    Added First_Program

---
+++ first_Program.py
@@ -0,0 +1,8 @@
+print("Hello World")    #display output on the screen
+print("This is the my First code in Python")
+print("I am learning Python programming.")
+print("My age  is 20 years.")
+print(26)   #displaying integer value
+print(3.14) #displaying float value
+input("Enter Your Favourite Number: ") #Input Function
+# This is a comment line in Python
